3da129b6813024a5f91a455c694587bcdd82c4e3
vietnq <khoahocmaytinh37@gmail.com>
2024-01-06 23:28:30 +0700

    commit code 6/1

--- src/main/java/com/example/cafe/Entity/Bill.java
+++ src/main/java/com/example/cafe/Entity/Bill.java
@@ -16,4 +16,68 @@ public class Bill implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
+
+    @Column(name = "uuid")
+    private String uuid;
+
+    @Column(name = "contactNumber")
+    private String contactNumber;
+
+    @Column(name = "paymentMethod")
+    private String paymentMethod;
+
+    @Column(name = "total")
+    private Integer total;
+
+    public Bill() {
+
+    }
+
+    public Bill(Integer id, String uuid, String contactNumber, String paymentMethod, Integer total) {
+        this.id = id;
+        this.uuid = uuid;
+        this.contactNumber = contactNumber;
+        this.paymentMethod = paymentMethod;
+        this.total = total;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
 }
